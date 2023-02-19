263196e225b02cbb2426bb22e8dcc822f66fad13
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-02-20 01:38:54 +0200

    Remove some styling from current Weather page

--- components/CurrentWeatherScreen.jsx
+++ components/CurrentWeatherScreen.jsx
@@ -88,7 +88,7 @@ const CurrentWeatherScreen = () => {
   }, [apikey, units, city]);
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView>
       {loading && <ActivityIndicator size="large" style={styles.loading} />}
       {!apikey && (
         <Text style={styles.error}>
@@ -122,11 +122,6 @@ const CurrentWeatherScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "lightgray",
-  },
   button: {
     margin: 10,
   },

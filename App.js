2699c141093eaab7ab4b68df99f3bb401d8fda25
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-02-19 13:36:32 +0200

    Added icons to button tab navigation

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import CurrentWeatherScreen from "./components/CurrentWeatherScreen";
 import ForecastScreen from "./components/WeatherForecastScreen";
 import SettingsScreen from "./components/SettingsScreen";
-
 import myDataContext from "./MyDataContext";
 
 const Tab = createBottomTabNavigator();
@@ -22,7 +21,26 @@ const App = () => {
       value={{ apikey, setApikey, units, setUnits, city, setCity }}
     >
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Current Weather") {
+                iconName = "cloud-outline";
+              } else if (route.name === "Forecast") {
+                iconName = "calendar-outline";
+              } else if (route.name === "Settings") {
+                iconName = "cog-outline";
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "blue",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
           <Tab.Screen name="Current Weather" component={CurrentWeatherScreen} />
           <Tab.Screen name="Forecast" component={ForecastScreen} />
           <Tab.Screen name="Settings" component={SettingsScreen} />
